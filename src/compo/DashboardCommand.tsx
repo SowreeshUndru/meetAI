e461b3ebeb95dd3cd754d20e5b7168829a38dae6
Your Name <you@example.com>
2025-07-14 19:38:04 +0530

    07:DashBoardNavbar

---
+++ src/compo/DashboardCommand.tsx
@@ -0,0 +1,18 @@
+import { CommandDialog, CommandInput, CommandItem, CommandList } from "@/components/ui/command";
+import { Dispatch, SetStateAction } from "react";
+
+interface props {
+  open: boolean;
+  setOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+export const DashboardCommand = ({ open, setOpen }: props) => {
+  return (
+    <CommandDialog open={open} onOpenChange={setOpen} className="max-w-lg w-full">
+      <CommandInput placeholder="Find a meeting or agent" />
+      <CommandList>
+        <CommandItem>Test</CommandItem>
+      </CommandList>
+    </CommandDialog>
+  );
+};
