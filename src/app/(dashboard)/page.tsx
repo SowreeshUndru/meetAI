d259291b7672653b97e9c843b0e401eb4e98ddec
Your Name <you@example.com>
2025-07-15 17:45:31 +0530

    08:trpc_setup

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,20 +1,19 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
-import LogoutButton from "@/compo/LogoutButton";
+"use client";
 
-export default async function Home() {
-  const session =await getServerSession();
-  console.log("hello");
-  if(!session){
-    redirect("/api/auth/signin");
-  }
+
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
+
+
+export default  function Home() {
+ const trpc=useTRPC();
+ const {data}=useQuery(trpc.hello.queryOptions({text:"sowreesh"}));
+
+
+
   return (
    <div>
-     <h1 className="text-2xl">Welcome, {session.user?.name || session.user?.email}!</h1>
-      <LogoutButton />
+      {data?.greeting || "Loading..."}
    </div>
-    
   );
 }
