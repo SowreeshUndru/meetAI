fe2ded1311d3e4aa47bb99ddcee6a18efb24adf7
Your Name <you@example.com>
2025-07-27 21:51:51 +0530

    12:addedFilters

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -4,30 +4,62 @@ import { TRPCError } from "@trpc/server";
 import { agentsInsertSchema } from "../schema";
 import { getServerSession } from "next-auth";
 
-import {z}  from "zod"
+import { z } from "zod"
 import { Sql } from "@/generated/prisma/runtime/library";
+import { Search } from "lucide-react";
+import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constant";
 const prisma = new PrismaClient();
 
 
 export const agentsRouter = createTRPCRouter({
-   getMany: baseProcedure.query(async ({ ctx }) => {
-      const data = await prisma.agent.findMany({});
-
-
-      return data;
+   getMany: protectedProcedure
+   .input(
+     z
+       .object({
+         page: z.number().default(DEFAULT_PAGE),
+         pageSize: z.number().min(MIN_PAGE_SIZE).max(MAX_PAGE_SIZE).default(DEFAULT_PAGE_SIZE),
+         search: z.string().nullish()
+       })
+       .optional()
+   )
+   .query(async ({ ctx, input }) => {
+     const page = input?.page ?? DEFAULT_PAGE;
+     const pageSize = input?.pageSize ?? DEFAULT_PAGE_SIZE;
+ 
+     const data = await prisma.agent.findMany({
+       where: {
+         userId: ctx.auth.user.id,
+         ...(input?.search
+           ? {
+               name: {
+                 contains: input.search,
+                 mode: 'insensitive', // Optional: case-insensitive search
+               },
+             }
+           : {}),
+       },
+       orderBy: [
+         { createdAt: 'desc' },
+         { id: 'desc' },
+       ],
+       skip: (page - 1) * pageSize,
+       take: pageSize,
+     });
+ 
+     return data;
    }),
+ 
+
+   getOne: baseProcedure
+      .input(z.object({
+         id: z.string(),
+      })).query(async ({ input }) => {
+         const data = prisma.agent.findFirst({ where: { id: input.id } });
+         return data;
+      }),
 
 
-   getOne:baseProcedure
-   .input(z.object({
-         id:z.string(),
-   })).query(async({input})=>{
-      const data=prisma.agent.findFirst({where:{id:input.id}});
-      return data;
-   }),
-   
-   
-   
+
 
    create: protectedProcedure
       .input(agentsInsertSchema)
@@ -36,7 +68,7 @@ export const agentsRouter = createTRPCRouter({
          const createdAgent = await prisma.agent.create({
             data: {
                ...input,
-               userId:ctx.auth.user.id
+               userId: ctx.auth.user.id
             }
          })
       }),
--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -1,25 +1,28 @@
 "use client"
 
 import { useTRPC } from "@/trpc/client";
-import {useSuspenseQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
 import Loading from "@/compo/Loading";
 import Error from "@/compo/Error";
 import { ResponsiveDialog } from "@/compo/ResponsiveDialog";
 import { Button } from "@/components/ui/button";
 import { DataTable } from "../components/data-table";
-import { columns} from "../components/columns";
+import { columns } from "../components/columns";
+import { useAgentFilters } from "../../hooks/use-agents-filters";
 
 export const AgentsView = () => {
- 
+    const [filters]=useAgentFilters()
     const trpc = useTRPC();
-    const { data,error} = useSuspenseQuery(trpc.agents.getMany.queryOptions());
+    const { data, error } = useSuspenseQuery(trpc.agents.getMany.queryOptions({
+        ...filters
+    }));
 
-    if(error) throw error;
-    
-    return(
+    if (error) throw error;
+
+    return (
         <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
-         
-          <DataTable data={data} columns={columns}  />
+
+            <DataTable data={data} columns={columns} />
         </div>
     )
 }
